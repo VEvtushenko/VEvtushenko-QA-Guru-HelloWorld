cde6344e7fd3c63ce6cdd196f98ffb0d0a68b7a2
VEvtushenko <evt-vladimir@yandex.ru>
2022-03-31 00:53:58 +0300

    init comit

---
+++ src/ru/yandex/evt/vladimir/Main.java
@@ -0,0 +1,10 @@
+package ru.yandex.evt.vladimir;
+
+import java.util.jar.JarOutputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
