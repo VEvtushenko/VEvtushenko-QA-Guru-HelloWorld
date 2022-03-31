74c13aceeed2691dd2f33c4604a84fc248e5e269
VEvtushenko <evt-vladimir@yandex.ru>
2022-03-31 23:46:24 +0300

    Remove anothe metod

--- src/ru/yandex/evt/vladimir/Main.java
+++ src/ru/yandex/evt/vladimir/Main.java
@@ -10,8 +10,5 @@ public class Main {
         System.out.println("Add new line");
         System.out.println("slow branch");
     }
-    
-    public static void another () {
-        System.out.println("Another metod");
-    }
+
 }
